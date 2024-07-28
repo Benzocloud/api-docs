b088398414ed2f2b0609e831b9c5b5e5429703fe
n3xus_ <whatnexus@vk.com>
2024-07-28 14:40:33 +0300

    upload

---
+++ components/layout/Breadcrumb.vue
@@ -0,0 +1,46 @@
+<template>
+  <UiBreadcrumb>
+    <UiBreadcrumbList>
+      <template v-for="(breadcrumb, index) in breadcrumbs" :key="breadcrumb.title">
+        <UiBreadcrumbItem>
+          <UiBreadcrumbLink
+            :href="index === 0 ? undefined : breadcrumb.href"
+            :class="{ 'text-foreground': index === breadcrumbs.length - 1 }"
+          >
+            {{ breadcrumb.title }}
+          </UiBreadcrumbLink>
+        </UiBreadcrumbItem>
+        <UiBreadcrumbSeparator v-if="index !== breadcrumbs.length - 1" />
+      </template>
+    </UiBreadcrumbList>
+  </UiBreadcrumb>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const { navigation } = useContent();
+
+interface Item {
+  title: string;
+  href: string;
+}
+
+function generateBreadcrumb(url: string): Item[] {
+  const breadcrumbItems: Item[] = [];
+  const segments = url.split('/').filter(segment => segment !== ''); // Remove empty segments
+
+  // Construct breadcrumb for each segment
+  let href = '';
+  let nav = navigation.value;
+  for (let i = 0; i < segments.length; i++) {
+    const segment = segments[i].replace('.html', '');
+    href += `/${segment}`;
+    const page = nav.find(x => (x._path as string) === href);
+    nav = page?.children;
+    breadcrumbItems.push({ title: page?.title ?? segment, href });
+  }
+  return breadcrumbItems;
+}
+
+const breadcrumbs = computed(() => generateBreadcrumb(route.path));
+</script>
